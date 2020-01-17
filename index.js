d93bb2cd24e5654550b38f457a9a5f2d1adea0fa
Wilson <wilsonj806@gmail.com>
2020-01-16 21:23:39 -0500

    Fix server file so it makes the API call correctly

--- index.js
+++ index.js
@@ -1,32 +1,43 @@
 const express = require('express');
 const http = require('http');
+const https = require('https');
 
 const app = express();
 
 app.get('/', (req, res, next) => {
   // make the app request this resource https://api.github.com/users/wilsonj806
-  const uri = 'https://api.github.com/users/wilsonj806';
-  // create a client on PORT 80 to connect to google
-  const clientToGithub = http.createClient(80, 'https://api.github.com/');
+  const uri = 'api.github.com';
+  const options = {
+    // Host is the API resource without the HTTP/ HTTPS protocol attatched AND without the path
+    host: uri,
+    agent: false,
+    // Path points to a particular resource you want to access
+    path: '/users/wilsonj806',
+    method: 'GET',
+    // Headers is the request header that the Express server will send
+    headers: { 'User-Agent': req.headers["user-agent"]}
+  };
 
-  // Start a request
-  const request = clientToGithub.request('GET', uri, { host: 'https://api.github.com/' });
-  // end request configuration/ send the request out
-  request.end();
+  https.get(options, function(apiRes) {
+    let data = '';
+    console.log('STATUS: ' + apiRes.statusCode);
+    // console.log('HEADERS: ' + JSON.stringify(apiRes.headers));
+    apiRes.setEncoding('utf8');
+    apiRes.on('data', function (chunk) {
+      console.log('BODY: ' + chunk);
+      data += chunk;
+    });
 
-  // wait for request response
-  request.on('response', (res) => {
-    // Log response metadata
-    console.log('STATUS: ' + res.status);
-    console.log('HEADERS: ' + JSON.stringify(res.headers));
-
-    // wait for response data
-    res.on('data', (chunk) => {
-      // log response data
-      console.log('RESPONSE DATA: \n', chunk);
+    apiRes.on('error', (error) => {
+      console.log('Error below \n\n\n');
+      console.log(error);
+      res.status(500).send('it broke')
     })
-  });
-  res.status(200).send("200 CALL SUCCESS");
+
+    apiRes.on('end', () => res.status(200).json(data))
+  })
 })
+
+
 
 app.listen(4000, () => console.log('Server running on PORT 4000'))
